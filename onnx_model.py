496f26fa67b7005c0aeb6bf00632fa499e640525
Belem Zhang <belem.zhang@intel.com>
2023-08-09 16:55:07 +0800

    add data type for ops

--- onnx_model.py
+++ onnx_model.py
@@ -6,30 +6,6 @@ import argparse
 import onnx
 import onnxruntime
 
-ONNX_DATA_TYPE_ID = {
-    'UNDEFINED': 0,
-    'FLOAT': 1,
-    'UINT8': 2,
-    'INT8': 3,
-    'UINT16': 4,
-    'INT16': 5,
-    'INT32': 6,
-    'INT64': 7,
-    'STRING': 8,
-    'BOOL': 9,
-    'FLOAT16': 10,
-    'DOUBLE': 11,
-    'UINT32': 12,
-    'UINT64': 13,
-    'COMPLEX64': 14,
-    'COMPLEX128': 15,
-    'BFLOAT16': 16,
-    'FLOAT8E4M3FN': 17,
-    'FLOAT8E4M3FNUZ': 18,
-    'FLOAT8E5M2': 19,
-    'FLOAT8E5M2FNUZ': 20
-}
-
 ONNX_DATA_TYPE = {
     0: 'undefined',
     1: 'float32',
@@ -160,7 +136,6 @@ def get_node_data_type(model: str):
                           ONNX_ATTRIBUTE_TYPE[ONNX_ATTRIBUTE_TYPE_ID[attr.type]])
                     file.write('  attribute ' + attr.name + ' ' +
                                ONNX_ATTRIBUTE_TYPE[ONNX_ATTRIBUTE_TYPE_ID[attr.type]] + '\n')
-
 
 def get_input_output(model: str):
     '''Get input / output data type of onnx models'''
